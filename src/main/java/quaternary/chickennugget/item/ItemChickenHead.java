9393de129fa8178f234938600f1064763539fbd3
quat <scratch933@gmail.com>
2019-02-21 19:33:48 -0500

    Add a little hint that you can wear the chicken head.

--- src/main/java/quaternary/chickennugget/item/ItemChickenHead.java
+++ src/main/java/quaternary/chickennugget/item/ItemChickenHead.java
@@ -1,5 +1,7 @@
 package quaternary.chickennugget.item;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityChicken;
@@ -10,9 +12,16 @@ import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import quaternary.chickennugget.ChickenNuggetCommonEvents;
 import quaternary.chickennugget.net.PacketUpdateChicken;
 import quaternary.chickennugget.block.BlockChickenHead;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemChickenHead extends ItemBlock {
 	public ItemChickenHead(BlockChickenHead block) {
@@ -40,5 +49,13 @@ public class ItemChickenHead extends ItemBlock {
 		} else {
 			return false;
 		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag mistake) {
+		super.addInformation(stack, worldIn, tooltip, mistake);
+		
+		tooltip.add(TextFormatting.DARK_GRAY.toString() + TextFormatting.ITALIC + I18n.format("item.chickennugget.chicken_head.hint"));
 	}
 }
